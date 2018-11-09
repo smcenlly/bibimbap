533fcdd5b9d3633ef5b77e02bee83b376b953446
Simon McEnlly <simon.mcenlly@gmail.com>
2018-11-09 11:02:05 +1000

    Update wallaby config to copy TypeScript node_modules/@types

--- wallaby.js
+++ wallaby.js
@@ -1,6 +1,7 @@
 module.exports = function() {
   return {
     files: [
+      { pattern:'node_modules/@types/**/*', instrument: false },
       'tsconfig.json',
       'src/**/*.ts',
       '!src/**/*.spec.ts'
